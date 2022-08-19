63fec3d8ba6a8027dbefb80e86105e4f8b057301
HaoZ124 <zhouhao927@gmail.com>
2022-08-18 21:24:20 -0600

    Added login and logout function, still need tests

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const flash = require('connect-flash');
 const ExpressError = require('./helpers/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
-const localStrategy = require('passport-local');
-const User = require('./Models/user');
-const campgrounds = require('./routers/campgrounds');
-const reviews = require('./routers/reviews');
-const users = require('./routers/users');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+
+const userRoutes = require('./routers/users');
+const campgroundRoutes = require('./routers/campgrounds');
+const reviewRoutes = require('./routers/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -50,23 +52,23 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new localStrategy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
 
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
-app.use('/', users);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 
 app.get('/', (req, res) => {
